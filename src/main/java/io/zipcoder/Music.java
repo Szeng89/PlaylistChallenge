d917c2c4c8f3ae84ca7a400ed2c58a3a26de869d
Szeng89 <shijinz898@gmail.com>
2024-03-18 23:56:16 -0400

    FInished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,23 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int index = 0;
+
+        // Loop through the playlist starting from one position after the startIndex because if current song was select we wouldnt get the right answer
+        for (int i = startIndex + 1; i < playList.length; i++) {
+            // Check if current song matches the selection
+            if (playList[i].equals(selection)) {
+                index = i;
+            }
+
+        }
+        int up = Math.abs(index - startIndex);
+        // Ensure positive number with Math.abs (absolute)
+        int down = Math.abs((startIndex + playList.length) - index);
+
+        // Return the smaller of the two distances up or down
+        return Math.min(up, down);
+
+
     }
 }
